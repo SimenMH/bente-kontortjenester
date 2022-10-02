e1e1fa5761f9142ac0bd3ea4925eebee31ecc707
SimenMH <simenmelo@gmail.com>
2022-10-02 18:39:38 +0200

    add smooth scrolling when clicking navbar

--- src/components/NavBar/NavBar.component.js
+++ src/components/NavBar/NavBar.component.js
@@ -1,33 +1,42 @@
 import './styles.css';
 
 function NavBar() {
+  const navigate = (e, id) => {
+    let el = document.getElementById(id);
+    e.preventDefault();
+    el && el.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='nav-container'>
       <div className='navbar'>
         <div className='logo-text'>Benko AS</div>
         <ul className='nav-menu'>
           <li>
-            <a href='#forside'>
+            <a href='#forside' onClick={e => navigate(e, 'forside')}>
               <p>Forside</p>
             </a>
           </li>
           <li>
-            <a href='#tjenester'>
+            <a href='#tjenester' onClick={e => navigate(e, 'tjenester')}>
               <p>Mine Tjenester</p>
             </a>
           </li>
           <li>
-            <a href='#kundeanbefalinger'>
+            <a
+              href='#kundeanbefalinger'
+              onClick={e => navigate(e, 'kundeanbefalinger')}
+            >
               <p>Kundeanbefalinger</p>
             </a>
           </li>
           <li>
-            <a href='#om-meg'>
+            <a href='#om-meg' onClick={e => navigate(e, 'om-meg')}>
               <p>Om Meg</p>
             </a>
           </li>
           <li>
-            <a href='#kontakt'>
+            <a href='#kontakt' onClick={e => navigate(e, 'kontakt')}>
               <p>Kontakt</p>
             </a>
           </li>
