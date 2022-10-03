222548fe7e3ad20181c335027ddbd0363983bec3
SimenMH <simenmelo@gmail.com>
2022-10-03 20:00:26 +0200

    add basic responsiveness

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -7,7 +7,12 @@ function Footer() {
         © 2022 BS Kontortjenester ved Bente Herland <br /> (+47) 123 45 678 |
         bente.s.herland@gmail.com
       </p>
-      <p>Org.nr 926 150 758</p>
+      <p>
+        Org.nr 926 150 758 <br />{' '}
+        <a href='https://www.flaticon.com/' title='icons'>
+          Icons made by Freepik / monkik / adriansyah from @flaticon
+        </a>
+      </p>
     </footer>
   );
 }
