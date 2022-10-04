08a19e383f5e148170d6420bd74c935d859dd3fc
Simen Herland <simen.herland@knowit.no>
2022-10-04 13:27:14 +0200

    remove phone number

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -4,7 +4,8 @@ function Footer() {
   return (
     <footer>
       <p>
-        © 2022 BS Kontortjenester ved Bente Herland <br /> (+47) 123 45 678 |
+        © 2022 BS Kontortjenester ved Bente Herland <br />{' '}
+        {/* (+47) 123 45 678 |*/}
         bente.s.herland@gmail.com
       </p>
       <p>
