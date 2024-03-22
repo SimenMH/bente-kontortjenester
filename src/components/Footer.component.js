b96cc4b687846412581fbe9d74803dfbdd05558e
SimenMH <simenmelo@gmail.com>
2024-03-22 18:42:31 +0100

    update org number

--- src/components/Footer.component.js
+++ src/components/Footer.component.js
@@ -2,8 +2,8 @@ function Footer() {
   return (
     <footer>
       <p>
-        © 2022 Benko av Bente Herland. Alle rettigheter forbeholdt. | Org.nr 926
-        150 758
+        © 2022 Benko av Bente Herland. Alle rettigheter forbeholdt. | Org.nr 925
+        931 497
       </p>
     </footer>
   );
